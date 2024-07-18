51ae87e607170d8dcf318a04743d887e28f5e511
dev-italosantos <italotayllor@gmail.com>
2024-07-18 12:18:49 -0300

    refactor: deleted script aulas

---
+++ aulas/aula20.py
@@ -0,0 +1,7 @@
+prim_value = input('Digite um valor: ')
+segu_valur = input('Digite outro valor: ')
+
+if prim_value >= segu_valur:
+    print(f'O primeiro valor {prim_value} é maior ou igual ao segundo')
+else:
+    print(f'O segundo valor {segu_valur} é maior ou igual ao primeiro')
