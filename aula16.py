5f8d39f06522e2f01a0703738e84f5c4ffb760ac
dev-italosantos <italotayllor@gmail.com>
2024-07-18 12:20:22 -0300

    refactor: deleted script aulas

--- aula16.py
+++
@@ -1,8 +0,0 @@
-entrada = input('Você quer "ficar" ou "ir embora"?')
-
-if entrada == 'ficar':
-    print('Você ficou no sisitema')
-elif entrada == 'embora':
-    print('Você saiu do sistema')
-else: 
-    print('Você não digitou nem ficar e nem ir embora')
